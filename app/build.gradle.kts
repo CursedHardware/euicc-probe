998f74871f64d01747d0aacb77c7ce2b178ad149
septs <github@septs.pw>
2024-08-24 16:10:41 +0800

    chore: upgrade gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "app.septs.euiccprobe"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
